31219014e8fc4c1d852abb9ad76e8b33a38e25ec
gfhe <heguangfu21@mails.ucas.ac.cn>
2023-08-29 01:58:20 +0000

    update init

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt install g++ cmake libboost-dev libgoogle-perftools-dev
-cd third_party/pybind11 && git submodule update --init --recursive && cd -
+git submodule update --init --recursive
+cd third_party/pybind11 && pip install .
 mkdir -p build && cd build
 cmake .. && make -j
